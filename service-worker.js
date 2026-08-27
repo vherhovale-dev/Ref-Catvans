d2301cc943a1fb8a12cc0ef58f07cc3fb331926f
vherhovale-dev <vherhovale@gmail.com>
2026-08-26 19:17:55 -0500

    Primera versión
    Hola como estas yo bien hol a

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+// Service Worker simple: cachea la app para que abra sin internet.
+const CACHE_NAME = 'referencias-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Estrategia: intenta red primero (para que veas cambios al reabrir),
+// si falla (sin internet) sirve la copia guardada en caché.
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
+  );
+});
